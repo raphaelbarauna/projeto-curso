2ec24004316c38a6428e4a445cac1bd7bd7facb4
raphael <rafael_baraunaa@hotmail.com>
2016-02-01 00:28:11 -0300

    Adicionar e remover tags

--- app/ProductTag.php
+++ app/ProductTag.php
@@ -4,11 +4,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductTag extends Model {
 
+protected $table      = 'product_tag';
+
+protected $primaryKey = 'tag_id';
+
     protected $fillable = [
         'product_id',
 	    'tag_id'
 	];	  
-		
+		 public $timestamps  = false;
 	public function product()
 	{
 		return $this->belongsTo('CodeCommerce\Product');
